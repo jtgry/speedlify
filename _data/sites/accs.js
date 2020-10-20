3cf197ec8d47c688b29482ea91e8700cc41cfe36
Tyler Gray <tyler@wearegray.co>
2020-10-19 22:33:34 -0500

    Create accs.js

---
+++ _data/sites/accs.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: "ACCS", // optional, falls back to object key
+	description: "ACCS",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://www.cacc.edu/",
+        "https://www.accs.edu/
+
+	]
+};
